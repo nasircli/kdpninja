547feba3f2c8efd5e3f358f546ce1fda8aa7e1ec
Nasir Uddin <43319931+nasircli@users.noreply.github.com>
2020-07-20 13:10:41 +0600

    Update main.js

--- main.js
+++ main.js
@@ -111,6 +111,18 @@ generate();
 
 document.addEventListener('contextmenu', event => event.preventDefault());
 
+// Change background by time
+var currentTime = new Date().getHours();
+if ( 7 <= currentTime && currentTime < 20) {
+    if (document.body) {
+        document.body.background = "#222";
+    }
+} else {
+    if (document.body) {
+        documetn.body.background = "https://i.gifer.com/Ir4.gif";
+    }
+}
+
 
 
 
